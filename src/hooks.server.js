8b640bb97a27a7150b6e24b065f84203ca5f40fa
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-07-25 21:12:19 +0300

    Used fn sequence from @sveltejs/kit/hooks.

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,15 +1,45 @@
 // @ts-nocheck
-export async function handle({ event, resolve }) {
+import { sequence } from '@sveltejs/kit/hooks';
+
+async function first ( { event, resolve } )
+{
 	event.locals.information = 'This text contains in the event.locals[name]';
-	// if(event.url.pathname === '/') return new Response('Hello EVENT');
-	return await resolve(event, {
-		transformPageChunk: ({ html }) => html.replace(
-			'<body',
-			'<body style="color: white; font-size: 1.5rem; background-color: #F9F4F4"'
-		)
-		//! ?????????????? transformPageChunk: ( { html } ) => html.replace( '%lang%', get_lang( event ) )
-	});
+	return await resolve( event, {
+		transformPageChunk: ( { html } ) => html.replace(
+				'<body',
+				'<body style="color: red; font-size: 1.5rem; background-color: #F9F4F4"'
+			),
+		preload: () => console.log('FIRST PRELOAD') //! how work it?
+	} )
 }
+async function second ( { event, resolve } )
+{
+	return await resolve( event, {
+		transformPageChunk: ( { html } ) => html.replace(
+				'<body',
+				'<body style="color: white; font-size: 1.5rem; background-color: #F9F4F4"'
+			),
+		preload: () => console.log( 'SECOND PRELOAD' ), //! how work it?
+		filterSerializedResponseHeaders: () => console.log( 'filterSerializedResponseHeaders' ) //! how work it?
+	} );
+}
+export const handle = sequence( first, second );
+
+
+
+//! handle
+// export async function handle ( { event, resolve } ) //! always only one on the hooks server!!!
+// {
+// 	event.locals.information = 'This text contains in the event.locals[name]';
+// 	// if(event.url.pathname === '/') return new Response('Hello EVENT');
+// 	return await resolve(event, {
+// 		transformPageChunk: ({ html }) => html.replace(
+// 			'<body',
+// 			'<body style="color: white; font-size: 1.5rem; background-color: #F9F4F4"'
+// 		)
+// 		//! ?????????????? transformPageChunk: ( { html } ) => html.replace( '%lang%', get_lang( event ) )
+// 	});
+// }
 
 //! Here you can for example redirect request
 // export async function handleFetch ( { request, fetch } ) {
