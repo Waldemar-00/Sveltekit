fb23a8a0c22024ce5c2893cb4cb161d07bf0fab8
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-07-27 18:50:07 +0300

    The use:enhance directive was used in the form element.

--- src/routes/form/+page.svelte
+++ src/routes/form/+page.svelte
@@ -1,5 +1,9 @@
 <script>
+	// @ts-nocheck
+
+	import { enhance } from '$app/forms';
 	export let data;
+	// $: console.log(data, 'DATA');
 
 	let comment = '';
 	export const snapshot = {
@@ -8,7 +12,21 @@
 	};
 </script>
 
-<form method="POST">
+<form
+	method="POST"
+	use:enhance={({ formElement, formData, action, submitter, cancel }) => {
+		// console.log(formElement, 'FORMELEMENT');
+		console.log(action, 'ACTION');
+		// console.log(submitter, 'SUBMITTER');
+		// console.log(cancel, 'CANCEL');
+		// console.log(formData, 'FORMDATA');
+		formData.set('comment', `${formData.get('comment')} ENHANCE`);
+		return async ({ result, update }) => {
+			console.log(result, 'RESULT', result?.status);
+			update();
+		};
+	}}
+>
 	<label for="comment">Your text</label>
 	<textarea name="comment" id="comment" bind:value={comment} placeholder="write your text here"
 	></textarea>
