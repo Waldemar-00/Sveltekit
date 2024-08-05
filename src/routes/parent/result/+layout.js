864e354332a37857daaf8fe6408f3217942ef1f3
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-08-05 20:56:51 +0300

    Used function untrck(()=>...) in the function load on the server(+layout.js).

--- src/routes/parent/result/+layout.js
+++ src/routes/parent/result/+layout.js
@@ -1,14 +1,15 @@
-// export async function load ({parent})
-// {
-//   // console.log(parent());
-//   const { a } = await parent();
-//   return { b: a + 1}
-// }
-
-export async function load ()
+export async function load ({parent, untrack})
 {
-  // return {
-  //   a: 13,
-  //   b: 7
-  // }
+  // console.log(parent());
+  const { a } = await parent();
+  untrack( () => console.log(a + 111) );
+  return { b: a + 11}
 }
+
+// export async function load ()
+// {
+//   // return {
+//   //   a: 13,
+//   //   b: 7
+//   // }
+// }
--- src/routes/parent/result/+page.svelte
+++ src/routes/parent/result/+page.svelte
@@ -5,9 +5,13 @@
 </script>
 
 <h1 style="--dynamic_color: {env.PUBLIC_DYNAMIC_COLOR}">{data.c}</h1>
+<h2>{data.a}</h2>
 
 <style>
 	h1 {
 		color: var(--dynamic_color);
+	}
+	h2 {
+		text-align: center;
 	}
 </style>
